42bc3891f82cab646a3fa28635bbcfc73f0c1a7a
root <erik@estreetworkshop>
2015-06-17 21:55:39 -0400

    Add Create SQL Backup

---
+++ Modules/createsqlbackup.php
@@ -0,0 +1,33 @@
+<?php
+	function Module_CreateSQLBackup($StatusFeed, $ServerName, $location, $dbname, $dbuser, $dbpass)
+	{
+		echo "----------------------------------------\n";
+		echo "Create SQL Backup...\n";
+		shell_exec("rm /backup/data/*.bak");
+		exec("mysqldump --lock-tables -h localhost -u " . $dbname . " -p". $dbpass. " " . $dbname . " > ". $location . "/".$dbname."-sqlbkp_`date +\"%Y%m%d\"`.bak 2>&1", $output, $ret);
+		
+		if( $ret !== 0 )
+		{		
+			echo "Adding RSS Item for SQL Backup Error\n";
+			//Create an empty FeedItem
+			$newItem = $StatusFeed->createNewItem();
+			  
+			//Add elements to the feed item
+			//Use wrapper functions to add common feed elements
+			$newItem->setTitle('SQL Backup Error');
+			$newItem->setLink("");
+			$newItem->setDate(time());
+			$newItem->setDescription("SQL Backup Error");
+			$newItem->addElement('author', $ServerName);
+			$newItem->addElement('guid', $ServerName ."SQLBackup" . time() . rand(), array('isPermaLink'=>'true'));
+			  
+			//Now add the feed item
+			$StatusFeed->addItem($newItem);
+		}
+		else			
+		{
+			echo "No SQL Error\n";
+			
+		}
+	}
+?>
